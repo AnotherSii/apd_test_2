ebf8d3246cf99f5ec6764ebf833709378e72ff86
AnotherSii <davidwillbedunn@gmail.com>
2021-01-03 00:52:00 -0800

    On branch main  Your branch is up to date with 'origin/main'. 	modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/components/Nav.svelte 	modified:   frontend/src/routes/_layout.svelte 	modified:   frontend/src/routes/index.svelte 	modified:   frontend/static/favicon.png 	modified:   frontend/static/global.css 	modified:   frontend/static/logo-192.png 	modified:   frontend/static/logo-512.png 	modified:   frontend/static/manifest.json

---
+++ frontend/old/src/routes/index.svelte
@@ -0,0 +1,25 @@
+<!-- frontend/src/routes/index.svelte -->
+<script>
+    import { goto } from '@sapper/app';
+    let user = "";
+    const login = () => { goto('/users/' + user); }
+</script>
+
+<style>
+   *{
+      text-align: center;
+   }
+</style>
+
+<svelte:head>
+   <title>Lamden Tutorial</title>
+</svelte:head>
+
+<h1>Lamden Tutorial</h1>
+
+
+<form on:submit|preventDefault={login}>
+   <p>Check your token balance</p>
+   <input type="text" bind:value={user} required="true"/>
+   <input class="button" type="submit" value="sign in" />
+</form>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,25 +1,50 @@
-<!-- frontend/src/routes/index.svelte -->
 <script>
-    import { goto } from '@sapper/app';
-    let user = "";
-    const login = () => { goto('/users/' + user); }
+	import successkid from 'images/successkid.jpg';
 </script>
 
 <style>
-   *{
-      text-align: center;
-   }
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
 </style>
 
 <svelte:head>
-   <title>Lamden Tutorial</title>
+	<title>Sapper project template</title>
 </svelte:head>
 
-<h1>Lamden Tutorial</h1>
+<h1>Great success!</h1>
 
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
 
-<form on:submit|preventDefault={login}>
-   <p>Check your token balance</p>
-   <input type="text" bind:value={user} required="true"/>
-   <input class="button" type="submit" value="sign in" />
-</form>
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
